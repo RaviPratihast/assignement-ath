01cc90a35964aa8ea7c8c50362df56f1babcb806
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2026-05-05 20:55:52 +0530

    Add HeroSection component to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Navbar } from "./components/Navbar";
+import { HeroSection } from "./components/HeroSection";
 
 function App() {
   return (
     <main className="min-h-screen bg-white">
       <Navbar />
+      <div className="mt-[8px]">
+        <HeroSection />
+      </div>
     </main>
   );
 }
